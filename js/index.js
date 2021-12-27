996aab6fe4ef79139c6d6b69b0c0317152fff965
nwagu chima <chima.nwagu@decagon.dev>
2021-12-27 11:13:49 +0100

    implementing smooth scrolling

--- js/index.js
+++ js/index.js
@@ -1,12 +1,40 @@
 const yearEl = document.querySelector(".year");
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const allLinks = document.querySelectorAll("a:link");
 
 // Setting current Year
 const year = new Date().getFullYear();
 yearEl.textContent = year;
 
+// Navigation
 btnNavEl.addEventListener("click", (e) => {
   e.preventDefault();
   headerEl.classList.toggle("nav-open");
+});
+
+// Smooth Scrolling Animation
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //   scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile nav
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
 });
